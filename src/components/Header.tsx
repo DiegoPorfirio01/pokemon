7b1c0bebed1bf6888d5c216e969e20498d94c271
Diego <porfiriodiego64@gmail.com>
2024-06-10 16:05:08 -0300

    refactor(Header) replace html  for better sintaxe header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,12 +4,12 @@ import Search from "./Search"
 const Header = () => {
   return (
     <>
-      <div className="flex items-center shadow-md h-24">
+      <header className="flex items-center shadow-md h-24">
           <div className="flex items-center justify-between w-full m-2 sm:m-36">
             <img src={Logo} alt="" width={200}/>
             <Search />
           </div>
-      </div>
+      </header>
     </>
   )
 }
